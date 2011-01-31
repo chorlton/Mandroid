39f572c8d5346aa070c64c9cb1c5a1a87198a700
unknown <glittle@.emea.convergys.com>
2011-01-31 11:34:27 +0000

    Panning working
    Added a one-touch zoom framework taken from sony ericsson tutorial. Panning is
    working with code in place for one-touch zoom.
    The Mandelbrot view parameters will be tracked in a separate state object,
    triggered to update when a finger is released.

--- src/org/gwl/android/mandroid/Mandroid.java
+++ src/org/gwl/android/mandroid/Mandroid.java
@@ -1,16 +1,33 @@
 package org.gwl.android.mandroid;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.View;
 
 public class Mandroid extends Activity {
+	
+	private PanZoomListener _touchListener;
+	private PanZoomState _panZoomState;
+	private Bitmap _bitmap;
+	private MandroidView _view;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         		
-		View v = new MandroidView(this);
-        setContentView(v);
+        setContentView(R.layout.main);
+        
+        _bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.title);
+
+        _panZoomState = new PanZoomState();
+        _touchListener = new PanZoomListener();
+        _touchListener.setPanZoomState(_panZoomState);
+        
+        _view = (MandroidView) findViewById(R.id.mandroidview);
+        _view.setPanZoomState(_panZoomState);
+        _view.setBitmap(_bitmap);
+        _view.setOnTouchListener(_touchListener);
     }
 }
--- src/org/gwl/android/mandroid/MandroidView.java
+++ src/org/gwl/android/mandroid/MandroidView.java
@@ -7,44 +7,40 @@ package org.gwl.android.mandroid;
 import java.util.Observable;
 import java.util.Observer;
 
+import org.gwl.android.mandroid.background.MandroidCreator;
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-//import android.view.MotionEvent;
 import android.os.Handler;
 import android.os.Message;
+import android.util.AttributeSet;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-//import android.view.View.OnTouchListener;
-import android.view.View.OnTouchListener;
 
 /**
  * 
  */
-public class MandroidView extends View implements Observer, OnTouchListener {
+public class MandroidView extends View implements Observer {
 	
 	private static final String TAG = MandroidView.class.getName();
 	
 	private Paint _textPaint = null;
 
 //	private int[] _buffer = null;
-	private ViewParams _viewParams;
-	private boolean _dirty = true;
-	private MandroidCreator _creator = null;
+	private PanZoomState _panZoomState;
+//	private boolean _dirty = true;
+//	private MandroidCreator _creator = null;
 	
 	private Bitmap _bitmap;
-	
-	private float _touchX;
-	private float _touchY;
-	
+		
 	private Handler _bitmapHandler = new Handler() {
 		public void handleMessage(Message msg) {
 			Log.d(TAG, "bitmapHandler.handleMessage()");
 			if(msg.what == MandroidCreator.FINISHED) {
 				setBitmap((Bitmap) msg.obj);
-				setDirty(false);
+//				setDirty(false);
 				invalidate();
 			}
 		}
@@ -60,41 +56,36 @@ public class MandroidView extends View implements Observer, OnTouchListener {
 		
 		setFocusable(true);
         setFocusableInTouchMode(true);
-
-        this.setOnTouchListener(this);
 	}
-
+	
+	public MandroidView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
 	/* (non-Javadoc)
 	 * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
 	 */
 	@Override
-	public void update(Observable creator, Object bitmap) {
+	public void update(Observable trigger, Object bitmap) {
 		Log.d(TAG, "update()");
-		Message msg = Message.obtain(_bitmapHandler, MandroidCreator.FINISHED, bitmap);
-		_bitmapHandler.sendMessage(msg);
+		if(trigger == _panZoomState) {
+			invalidate();
+		}
+		else {
+			Message msg = Message.obtain(_bitmapHandler, MandroidCreator.FINISHED, bitmap);
+			_bitmapHandler.sendMessage(msg);
+		}
 	}
 	
-	/* (non-Javadoc)
-	 * @see android.view.View.OnTouchListener#onTouch(android.view.View, android.view.MotionEvent)
-	 */
-/*	@Override
-	public boolean onTouch(View view, MotionEvent event) {
-		int r = (int) (256 * event.getX() / getWidth());
-		int g = (int) (256 * event.getY() / getHeight()); 
-		_color = (255 << 24) + (r << 16) + (g << 8) + 255;
-		invalidate();
-		return true;
-	}
-*/
-	private synchronized boolean isDirty() {
-		return _dirty;
-	}
+//	private synchronized boolean isDirty() {
+//		return _dirty;
+//	}
 	
-	private synchronized void setDirty(boolean dirty) {
-		_dirty = dirty;
-	}
+//	private synchronized void setDirty(boolean dirty) {
+//		_dirty = dirty;
+//	}
 	
-	private synchronized void setBitmap(Bitmap bitmap) {
+	public synchronized void setBitmap(Bitmap bitmap) {
 		_bitmap = bitmap;
 	}
 	
@@ -102,40 +93,41 @@ public class MandroidView extends View implements Observer, OnTouchListener {
 		return _bitmap;
 	}
 	
-	private void initViewParams() {
-		Log.d(TAG, "Initialising view parameters");
-		if(_viewParams == null) {
-			_viewParams = new ViewParams(getWidth(), getHeight());
+//	private void initViewParams() {
+//		Log.d(TAG, "Initialising view parameters");
+//		if(_state == null) {
+//			_state = new ViewState(getWidth(), getHeight());
+//		}
+//	}
+	
+	public void setPanZoomState(PanZoomState panZoomState) {
+		if(_panZoomState != null) {
+			_panZoomState.deleteObserver(this);
 		}
+		
+		_panZoomState = panZoomState;
+		_panZoomState.addObserver(this);
 	}
 	
-/*	private synchronized int[] getBuffer() {
-		return _buffer;
-	}
-	
-	private synchronized void setBuffer(int[] buffer) {
-		_buffer = buffer;
-	}*/
-	
-	private void generateBitmap() {
+/*	private void generateBitmap() {
 		Log.d(TAG, "generateBitmap()");
 		initViewParams();
 		// fire and forget
-		_creator = new MandroidCreator(_viewParams);
+//		_creator = new MandroidCreator(_state);
 		_creator.addObserver(this);
 		Thread thread = new Thread(_creator, "mandcreator");
 		thread.start();
-	}
+	}*/
 	
 	@Override
 	public void onDraw(Canvas canvas) {
 		Log.d(TAG, "onDraw()");
-		if(isDirty()) {
+/*		if(isDirty()) {
 			generateBitmap();
 		}
-		else {
-			canvas.drawBitmap(getBitmap(), 0.0f, 0.0f, _textPaint);				
-		}
+		else {*/
+			canvas.drawBitmap(getBitmap(), _panZoomState.getPanX(), _panZoomState.getPanY(), _textPaint);				
+		/*}*/
 	}
 	
 	private Paint initaliseTextPaint() {
@@ -146,32 +138,4 @@ public class MandroidView extends View implements Observer, OnTouchListener {
 		return p;
 	}
 
-	/* (non-Javadoc)
-	 * @see android.view.View.OnTouchListener#onTouch(android.view.View, android.view.MotionEvent)
-	 */
-	@Override
-	public boolean onTouch(View v, MotionEvent event) {
-		Log.d(TAG, "onTouch");
-		switch(event.getAction()) {
-		case MotionEvent.ACTION_DOWN:
-			_touchX = event.getX();
-			_touchY = event.getY();
-			Log.d(TAG, "DOWN: " + _touchX + ", " + _touchY);
-			Log.d(TAG, "reim: " + _viewParams.x2re((int) _touchX) + ", " + _viewParams.y2im((int) _touchY));
-			break;
-		case MotionEvent.ACTION_UP:
-			Log.d(TAG, "UP: " + event.getX() + ", " + event.getY());
-			Log.d(TAG, "reim: " + _viewParams.x2re((int) event.getX()) + ", " + _viewParams.y2im((int) event.getY()));
-			_viewParams = new ViewParams(_viewParams.getWidth(),
-					_viewParams.getHeight(),
-					_viewParams.getReal() + _viewParams.x2re((int) _touchX) - _viewParams.x2re((int) event.getX()),
-					_viewParams.getImag() + _viewParams.y2im((int) _touchY) - _viewParams.y2im((int) event.getY()),
-					_viewParams.getScale());
-			setDirty(true);
-			invalidate();
-			break;
-		}
-					
-		return true;
-	}
 }
